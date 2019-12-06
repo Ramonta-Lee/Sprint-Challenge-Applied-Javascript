bf7492a0c7768d4360957f4735bf58c25ce6aa13
unknown <viola4lfe@gmail.com>
2019-12-06 13:39:03 -0500

    rough code of header, tabs, cards

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const tabEntry = document.querySelector('.topics')
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+ .then(response => {
+  const dataObj = response.data.topics.forEach(item => {
+   tabEntry.appendChild(TabComponent(item));
+ 
+  });
+  console.log(response)
+
+ })
+
+ function TabComponent (data){
+  //define elements
+  const tabDiv = document.createElement('div');
+
+  // set class names
+  tabDiv.classList.add('tab');
+
+  // set text content
+  tabDiv.textContent = data;
+
+  // set up structure
+  
+
+  return tabDiv;
+ }
